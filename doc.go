01228e5e5c2c522c7b5f3b91269287a761367110
Nate Finch <nate.finch@gmail.com>
2014-03-29 07:19:51 -0400

    more

--- doc.go
+++ doc.go
@@ -2,7 +2,7 @@
 
 // Command godocgo is a repo demonstrating effective Go documentation.
 //
-//   Author: Nate Finch, March 29, 2014
+// Introduction
 //
 // I started to write this as a blog post, intending to link to a repo with
 // examples, and then realized that there was no reason I couldn't write the
@@ -18,9 +18,8 @@
 // auto-generated documentation for your code.  If you're coming from javadoc or
 // similar tools, you're probably thinking one of two things (possibly both):
 //
-// 1) meh, we have that
-//
-// 2) gah! no!
+//   1) meh, we have that
+//   2) gah! no!
 //
 // An understandable reaction, but let me show you why godoc is different, and
 // how to leverage all its features.
@@ -32,34 +31,42 @@
 // bracket to be found.  Simple conventions replace the html/xml tags you often
 // see in other documentation standards.
 //
-//   Formatting
+// Formatting
 //
-// The line above above this is pre-formatted text.  All that is required for it
-// to be rendered in that way is that it be indented from the surrounding
-// comments (you can use tabs or spaces, whatever you like).  This is handy for
-// showing code snippets that you don't want godoc to auto-wrap, the way it does
-// normal text.
+// The line above above this is heading. It is simply a single line without
+// punctuation with paragraphs before and after it.  As you can see, it gets
+// special formatting and is included in the table of contents at the top.
 //
-//	a := b
-//	for c := range a {
-//		fmt.Println(c)
+// Below this is pre-formatted text.  All that is required for it to be rendered
+// in that way is that it be indented from the surrounding comments (you can use
+// tabs or spaces, whatever you like).  This is handy for showing code snippets
+// that you don't want godoc to auto-wrap, the way it does normal text.
+//
+//	a := []string{"foo", "bar"}
+//	for _, b := range a {
+//		fmt.Println(b)
 //	}
 //
 // Html links will be auto linked without you having to do anything, like so:
 // http://golang.org
 //
-// Finally, godoc allows you to embed example code in the documentation that is
-// actually run and the output tested when you run go test.  This is fantastic
-// feature that ensures your examples will never get out of sync with the
-// functional code. More about that later.
+// That's it, that's all the formatting you get.  But it's enough, and I think
+// we can all be glad to focus on the content and not the formatting of our
+// documentation.
 //
-//   This Text
+// This Text
 //
 // The text you are reading is long form documentation on the main package of a
 // Go executable.  The Go authors were aware that you might need a really long
 // comment on your application, so they devoted an entire file to it - doc.go.
 // The contents of the file are generally just a really long comment, and the
-// package declaration "package main".  Note that this only applies to the main
-// package of applications, not for library packages. We'll get to those in a
-// minute.
+// package declaration "package main".  You can also have code in doc.go, but
+// traditionally it only contains documentation.
+//
+// Note that doc.go only applies to the main package of applications, not for
+// library packages.
+//
+// When you run godoc on your own machine
+// To read more of the article, and exmplore more of godoc,
+// click on the link to the package named "sub" below.
 package main
