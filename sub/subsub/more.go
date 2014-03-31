96e6ff5f1b5994c657577a052e8943bbf37896d7
Nate Finch <nate.finch@gmail.com>
2014-03-31 11:44:17 -0400

    add some wrap up at the end.

--- sub/subsub/more.go
+++ sub/subsub/more.go
@@ -1,4 +1,14 @@
-// Package subsub just shows how navigation works for packages under other packages.
+// Package subsub wraps up the article.
 //
-// Nothing more to see, you've read everything, congrats!
+// This is the end of the article/repo whatever you want to call it.  I hope you
+// learned something.  I certainly did just by writing it all down and doing
+// some experimenting to make sure what I thought was true actually was true.
+//
+// Feel free to send a pull request if you think something can be clarified or
+// if I missed anything.
+//
+// Remember - documentation is how you keep other people from bugging you about
+// your code.  It's also an opportunity to explain your rationale behind the API
+// you have chosen to expose.  Use examples liberally, often times the functions
+// and types alone don't properly convey how to use your code.
 package subsub
