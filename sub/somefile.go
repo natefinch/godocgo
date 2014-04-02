c5459913169eb9f625eac15d798ea078c3ccc213
Nate Finch <nate.finch@gmail.com>
2014-04-02 07:42:08 -0400

    a little more description about bugs, and note that they don't show up on the command in godoc.org

--- sub/somefile.go
+++ sub/somefile.go
@@ -1,11 +1,16 @@
 package sub
 
-// BUG(natefinch) This is a bug description.  Bugs are an exception to the rule
+// BUG(natefinch) This is a bug description. Bugs are an exception to the rule
 // about only attached comments getting displayed in godoc.  Simply make a
 // comment that starts with BUG(foo) where foo is the name of the person
-// responsible for the bug, and it'll show up in the list of bugs in godoc.  You
-// can click on the link at the beginning of the bug description to go right to
-// the place where the BUG comment is located in the code.
+// responsible for the bug, and it'll show up in the list of bugs in godoc.
+// Note that bug descriptions are plaintext only.  You can click on the link at
+// the beginning of the bug description to go right to the place where the BUG
+// comment is located in the code.
+
+// BUG(godoc.org) Note that bugs in the command (application) package do not
+// show up on godoc.org.  This appears to be a bug in godoc.org, as they do show
+// up in local godoc.
 
 // Constants are sorted to the top of the code documentation.
 //
