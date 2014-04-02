1558edc9b28c4ef766538db3f77801daf6ff2dd0
Nate Finch <nate.finch@gmail.com>
2014-04-02 07:27:58 -0400

    add a bug to show bug docs

--- main.go
+++ main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+// BUG(natefinch) This is a bug description.  Bugs are an exception to the rule
+// about only attached comments getting displayed in godoc.  Simply make a
+// comment that starts with BUG(foo) where foo is the name of the person
+// responsible for the bug, and it'll show up in the list of bugs in godoc.  You
+// can click on the link at the beginning of the bug description to go right to
+// the place where the BUG comment is located in the code.
+
 func main() {
 	fmt.Println("Hello world!")
 }
